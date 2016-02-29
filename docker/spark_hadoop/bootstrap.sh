d3b9bdd3bc0df91c2b027c1274feb5e7b144c1ad
YeongWei <laiyw@persistent.my>
2016-02-29 02:11:37 -0500

    Move common EXPORTS into npid

--- docker/spark_hadoop/bootstrap.sh
+++ docker/spark_hadoop/bootstrap.sh
@@ -23,7 +23,9 @@ trap "term_handler;exit" SIGTERM SIGINT
 
 (cd $NPI_HOME/bin && ./npid start hadoop)
 
-readPids() {	cat ${1} | tr '\n' ' '	}
+readPids() {	
+	cat ${1} | tr '\n' ' '	
+}
 PIDS="`readPids $NPI_HOME/var/hadoop*.pid`"
 PIDS="${PIDS}`readPids $NPI_HOME/var/yarn*.pid`"
 
